5f41bcd0b34e2b3d78088c9f73d5e8aac2ed4cde
XRayCRC <chrisconrad9921@gmail.com>
2023-02-17 11:34:06 -0500

    first commit

---
+++ MyAvgLib/Avg.cs
@@ -0,0 +1,19 @@
+﻿namespace MyAvgLib
+{
+    public class Avg
+    {
+        public static double Average(double a, double b)
+        {
+            double average;
+            average = (a + b) / 2;
+            return average;
+        }
+
+        public static double Average(double a, double b, double c)
+        {
+            double average;
+            average = (a + b + c) / 3;
+            return average;
+        }
+    }
+}
---
+++ MyAvgLibTest/UnitTest1.cs
@@ -0,0 +1,29 @@
+using MyAvgLib;
+namespace MyAvgLibTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void TestAvg3()
+    {
+        double a = 2; //Arrange
+        double b = 10;
+        double c = 48;
+        double expected = 20;
+
+        double actual = MyAvgLib.Avg.Average(a, b, c); //Act
+        Assert.Equal(expected, actual); //Assert
+
+    }
+
+    [Fact]
+    public void TestAvg2()
+    {
+        double a = 33; //Arrange
+        double b = 18;
+        double expected = 25.5;
+
+        double actual = MyAvgLib.Avg.Average(a, b); //Act
+        Assert.Equal(expected, actual); //Assert
+    }
+}
